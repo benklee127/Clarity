8a6a379044dc03549c59e9c12c835c687b9815c3
juniper <benklee127@gmail.com>
2023-07-24 09:42:08 -0400

    added some seeder data

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -5,17 +5,23 @@ from sqlalchemy.sql import text
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        username='Demo', email='demo@aa.io', password='password', first_name='demo', last_name='lition', profile_photo="https://www.thesprucepets.com/thmb/9zz13o5HFZcGtBQOnwq1jZ-ajDQ=/2000x0/filters:no_upscale():strip_icc()/savannah-cat-4ec08e9efe40457a8f73a2af24fcdcc3.jpeg")
+        username='Demo', email='demo@aa.io', password='password', first_name='Demo', last_name='Lition', profile_photo="https://www.thesprucepets.com/thmb/9zz13o5HFZcGtBQOnwq1jZ-ajDQ=/2000x0/filters:no_upscale():strip_icc()/savannah-cat-4ec08e9efe40457a8f73a2af24fcdcc3.jpeg")
     marnie = User(
-        username='marnie', email='marnie@aa.io', password='password',first_name='mary', last_name='arnie',profile_photo="https://images.wagwalkingweb.com/media/care/hero/1632522661.8813548/sergey-semin-i9chfdylt3e-unsplash.jpg")
+        username='marnie', email='marnie@aa.io', password='password',first_name='Mary', last_name='Murray',profile_photo="https://images.wagwalkingweb.com/media/care/hero/1632522661.8813548/sergey-semin-i9chfdylt3e-unsplash.jpg")
     bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password',first_name='robert', last_name='deniro', profile_photo='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM50JpAefCcT9Alyo8H_v5iqsBbNB6A5CXD8Jgggs3UePzvzA9onL7ULKxBJ4JZxeWsSc&usqp=CAU')
+        username='bobbie', email='bobbie@aa.io', password='password',first_name='Carter', last_name='Fuller', profile_photo='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM50JpAefCcT9Alyo8H_v5iqsBbNB6A5CXD8Jgggs3UePzvzA9onL7ULKxBJ4JZxeWsSc&usqp=CAU')
     sam = User(
-        username='sam', email='sam@aa.io', password='password',first_name='sam', last_name='uel')
+        username='sam', email='sam@aa.io', password='password',first_name='Tricia', last_name='Palmer', profile_photo='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxzH8mYfe6Xlonm6sX4isVIAbLtp2cCpRwLg&usqp=CAU')
+    user5 = User(
+        username='albert', email='albert.murray@example.com', password='password',first_name='Jerry', last_name='Morales', profile_photo='https://randomuser.me/api/portraits/men/75.jpg')
+    user6 = User(
+        username='joygriffin', email='oy.griffin@example.com', password='password',first_name='Joy', last_name='Griffen', profile_photo='https://randomuser.me/api/portraits/women/54.jpg')
     db.session.add(demo)
     db.session.add(marnie)
     db.session.add(bobbie)
     db.session.add(sam)
+    db.session.add(user5)
+    db.session.add(user6)
     db.session.commit()
 
 
